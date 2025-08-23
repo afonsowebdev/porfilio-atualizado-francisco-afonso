baba8e2d72a614232ba0478a0afc29f94621a53f
Francisco Afonso <afonso.webdev@gmail.com>
2025-08-23 11:34:24 +0100

    Alteração de códigos inacabado

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,25 +22,6 @@ prevBtn.addEventListener("click", () => {
 
 // Modo Dark
 
-/* // Alternar modo dark
-const toggleDark = document.getElementById("toggle-dark");
-
-toggleDark.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  // Salvar preferência no localStorage
-  const isDark = document.body.classList.contains("dark-mode");
-  localStorage.setItem("modo-escuro", isDark);
-});
-
-// Aplicar preferência salva
-window.addEventListener("DOMContentLoaded", () => {
-  const preferencia = localStorage.getItem("modo-escuro");
-  if (preferencia === "true") {
-    document.body.classList.add("dark-mode");
-  }
-}); */
-
 const toggleDark = document.getElementById("toggle-dark");
 const icon = document.getElementById("icon-dark");
 
@@ -94,27 +75,6 @@ const observer = new IntersectionObserver(
 // Observar todos os elementos com a classe
 animatedElements.forEach((el) => observer.observe(el));
 
-/* // Scroll suave personalizado
-const linksInternos = document.querySelectorAll('a[href^="#"]');
-
-linksInternos.forEach((link) => {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = this.getAttribute("href");
-
-    if (href.length > 1) {
-      // Ignora href="#"
-      const destino = document.querySelector(href);
-      if (!destino) return;
-
-      destino.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  });
-});
- */
 
 // ====== Scroll para o topo ======
 const scrollToTopBtn = document.getElementById("scroll-up");
