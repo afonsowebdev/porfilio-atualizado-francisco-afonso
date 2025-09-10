d31c9c4b122ed95fcc0fee031975bfddf5a72404
Francisco Afonso <afonso.webdev@gmail.com>
2025-09-10 22:26:26 +0100

    Alterações concluidas

---
+++ assets/js/btnDownload.js
@@ -0,0 +1,7 @@
+addEventListener('DOMContentLoaded', () => { 
+    const btnDownload = document.getElementById('btnDownload');
+
+    btnDownload.addEventListener('click', () => {
+        alert('Obrigado por baixar meu currículo! Se precisar de mais informações, não hesite em entrar em contato.');
+    });
+});
